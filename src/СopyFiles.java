e0aafb875517eb5b7c8359b67295205748d752a3
Boris Bovin <b.bovin@mail.ru>
2019-09-15 18:49:02 +0300

    fix folder creation logic in CopyFiles class

--- src/СopyFiles.java
+++ src/СopyFiles.java
@@ -6,29 +6,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
 
-public class copyFiles {
+public class СopyFiles {
 
   public static void main(String[] args) throws Exception {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     for (; ; ) {
-      System.out.println("Введите путь к папке которую хотите скопировать");
-      Path inputPath = Paths.get(reader.readLine());
-      if (!inputPath.toFile().exists()) {
-        System.out.println("Файл не существует");
+      System.out.println("Введите путь к папке которую хотите скопировать:");
+      File sourceFile = new File(reader.readLine());
+      if (!sourceFile.exists()) {
+        System.out.println("Файл не существует!");
         continue;
       }
-      System.out.println("Введите путь куда скопировать");
-      Path outputPath = Paths.get(reader.readLine() + File.separator + inputPath.getFileName().toString());
-      copyFile(inputPath, outputPath);
+      System.out.println("Введите путь куда скопировать:");
+      File destinationFile = new File(reader.readLine() + File.separator + sourceFile.getName());
+      if (!destinationFile.exists()) destinationFile.mkdirs();
+
+      copyFile(sourceFile.toPath(), destinationFile.toPath());
     }
   }
 
-  private static void copyFile(Path inputPath, Path outputPath) throws IOException {
+  private static void copyFile(Path inputPath, Path outputPath)  {
+    try {
     List<Path> sources = Files.walk(inputPath).collect(toList());
     List<Path> destinations = sources.stream()
         .map(inputPath::relativize)
@@ -39,6 +41,9 @@ public class copyFiles {
       Files.copy(sources.get(i), destinations.get(i), StandardCopyOption.REPLACE_EXISTING);
     }
     System.out.println("Файлы скопированы!");
+  }catch (IOException e){
+      e.printStackTrace();
+    }
   }
 }
 
